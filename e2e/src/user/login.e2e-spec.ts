30ed2a8b45aaa5168237dc5e8ff6675b1d2db966
拉拉醬 <lala.lee.goo@gmail.com>
2019-04-28 11:42:30 +0800

    完成登入功能的 E2E 測試 成功與失敗

--- e2e/src/user/login.e2e-spec.ts
+++ e2e/src/user/login.e2e-spec.ts
@@ -1,3 +1,19 @@
-describe('the user try to login', () => {
+import { browser, element, by} from 'protractor';
 
+describe('the user try to login', () => {
+  it('should login to event page', () => {
+    browser.get('/user/login');
+    element(by.id('userName')).sendKeys('John');
+    element(by.id('password')).sendKeys('123456');
+    element(by.buttonText('登入')).click();
+    expect(browser.getCurrentUrl()).toContain('events');
+  });
+  it('should login fail', () => {
+    browser.get('/user/login');
+    element(by.id('userName')).sendKeys('John');
+    element(by.id('password')).sendKeys('654321');
+    element(by.buttonText('登入')).click();
+    let fail = element(by.css('.alert-danger'));
+    expect(fail.getText()).toEqual('錯誤的帳號密碼');
+  });
 });
